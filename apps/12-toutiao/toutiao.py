42e9ebda89c66e42ee6bb2b1e8c1cc9610d181e3
fslong <fsl470657570@outlook.com>
2018-09-05 13:42:44 +0800

    头条街拍

--- apps/12-toutiao/toutiao.py
+++ apps/12-toutiao/toutiao.py
@@ -7,6 +7,11 @@ __author__ = 'fslong'
 
 import requests
 import pymongo
+import os
+import traceback
+import hashlib
+import asyncio
+import pymongo
 
 
 class Toutiao(object):
@@ -27,22 +32,103 @@ class Toutiao(object):
 
         }
         self.jsons = []
-        self.result=[]
+        self.result = []
 
     def getPage(self, offset):
         self.params['offset'] = offset
         print(self.params)
         try:
-            response = requests.get(url=self.baseUrl, headers=self.headers)
+            response = requests.get(
+                url=self.baseUrl, params=self.params, headers=self.headers)
             if response.status_code == 200:
-                self.jsons.append(response.json())
+                return(response.json())
         except requests.ConnectionError as e:
             print(e)
-    
-    def getImages(self):
-        for json in self.jsons:
-            if json.get('data'):
-                for item in json.get('title'):
-                    images=item.get('')
-                
 
+    def getImages(self, dataJson):
+        if dataJson.get('data'):
+            for item in dataJson.get('data'):
+                if item.get('image_list'):
+                    images = item.get('image_list')
+                    title = item.get('title').split('，')[0].split('：')[0]
+                    if title.endswith('街拍'):
+                        title = '街拍'+title.split('街拍')[0]
+                    self.save2mongo(item)
+                    for image in images:
+                        url = image.get('url')
+                        if url.endswith('_tt'):
+                            url = 'http://p9.pstatp.com/large/tuchong.fullscreen/' + \
+                                url.split('/')[-1]
+                        else:
+                            url = 'http:'+url.replace('list', 'large')
+                        yield{
+                            'image': url,
+                            'title': title
+                        }
+                elif item.get('display'):
+                    data = item.get('display')
+                    if data.get('items'):
+                        data = data.get('items')
+                        for image in data:
+                            title = image.get('reason').split('，')[
+                                0].split('：')[0]
+                            if title.endswith('街拍'):
+                                title = '街拍'+title.split('街拍')[0]
+                            image['title'] = title
+                            self.save2mongo(image)
+                            yield{
+                                'image': image.get('img'),
+                                'title': title
+                            }
+
+    def saveImage(self, item):
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'img')
+        path = os.path.join(path, item.get('title'))
+        if not os.path.exists(path):
+            os.mkdir(path)
+        try:
+            response = requests.get(item.get('image'), headers=self.headers)
+            if response.status_code == 200:                
+                file_path = os.path.join(path, '{0}.{1}'.format(hashlib.md5(
+                    response.content).hexdigest(), 'jpg'))
+                if not os.path.exists(file_path):
+                    with open(file_path, 'wb') as f:
+                        f.write(response.content)
+                else:
+                    print('好奇怪，这个图片已经下完了！')
+        except:
+            traceback.print_exc()
+
+    def save2mongo(self, jsonData):
+        if jsonData.get('data'):
+            mongo = pymongo.MongoClient(host='localhost', port=27017)
+            db = mongo['toutiao']
+            collector = db['jiepai']
+            for data in jsonData.get('data'):
+                if collector.find_one(data):
+                    print('《%s》的数据已经在数据库中存在！' % data.get('title'))
+                else:
+                    try:
+                        collector.insert_one(data)
+                        print('《%s》存储完毕！' % data.get('title'))
+                    except:
+                        traceback.print_exc()
+
+    async def main(self, offset):
+        jsonData = self.getPage(offset)
+        self.save2mongo(jsonData)
+        for item in self.getImages(jsonData):
+            print(item)
+            self.saveImage(item)
+
+
+GROUP_START = 0
+GROUP_END = 20
+
+if __name__ == '__main__':
+    toutiao = Toutiao()
+    loop = asyncio.get_event_loop()
+    tasks = [toutiao.main(offset*20)
+             for offset in range(GROUP_START, GROUP_END)]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
